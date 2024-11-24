9fd2402970f4cf526cf6af3c291bb1d617aef70d
GJZ26 <adolfogj26@gmail.com>
2024-11-23 23:31:36 -0600

    feat: Adding visual assets

--- src/main/java/com/upchiapas/render/Renderer.java
+++ src/main/java/com/upchiapas/render/Renderer.java
@@ -3,12 +3,17 @@ package com.upchiapas.render;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Renderer extends GameApplication {
     @Override
     protected void initSettings(GameSettings settings) {
-        settings.setWidth(800);
-        settings.setHeight(600);
-        settings.setFullScreenAllowed(false);
+
+        settings.setWidth(700);
+        settings.setHeight(700);
+        settings.setFullScreenAllowed(true);
+        settings.setGameMenuEnabled(false);
         settings.setVersion("1.0.0");
         settings.setTitle("Kitchen Simulator");
     }
